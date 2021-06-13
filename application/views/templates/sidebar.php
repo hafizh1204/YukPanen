b2645e00f095314043089a819581f0a28c2ee2c4
hafizh1204 <75535386+hafizh1204@users.noreply.github.com>
2021-06-13 20:50:54 +0700

    Update sidebar.php

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -115,7 +115,7 @@
                                             placeholder="Search for..." aria-label="Search"
                                             aria-describedby="basic-addon2">
                                         <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
+                                            <button class="btn btn-success" type="button">
                                                 <i class="fas fa-search fa-sm"></i>
                                             </button>
                                         </div>
